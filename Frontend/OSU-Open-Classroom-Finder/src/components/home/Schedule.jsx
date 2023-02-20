672f8a0ceb2513765de80ee1eb07fa1b5e9dd669
Samiul Islam <cs.isamiul@gmail.com>
2023-02-20 13:22:51 -0500

    Started work on calandarHelper

--- Frontend/OSU-Open-Classroom-Finder/src/components/home/Schedule.jsx
+++ Frontend/OSU-Open-Classroom-Finder/src/components/home/Schedule.jsx
@@ -1,4 +1,4 @@
-import {pageHeightEm, maxHeightEm, minHeightEm} from "../../helpers/calandarHelpers"
+import {pageHeightEm, maxHeightEm, minHeightEm, calculateItemPosition} from "../../helpers/calandarHelpers";
 
 function Schedule({ date }) {//, states, setStates }) {
     //useContext and useCallback to update states.dayTimes
@@ -6,11 +6,13 @@ function Schedule({ date }) {//, states, setStates }) {
     states.dayTimes[`start${date}`] = "10:00AM";
     states.dayTimes[`end${date}`] = "1:00PM";
     const room = "BE128";
-    const roomStart = "11:00PM";
+    const roomStart = "11:00AM";
     const roomEnd = "12:30PM";
 
+    calculateItemPosition(states.dayTimes[`start${date}`], states.dayTimes[`end${date}`], roomStart);
+    
     return <>
-        <div style={{ outlineStyle: "solid", height: `${pageHeightEm}` }}>
+        <div style={{ outlineStyle: "solid", height: `${pageHeightEm}em` }}>
             {/* Start time and day of the week */}
             <div>
                 {date} <br />
@@ -18,12 +20,12 @@ function Schedule({ date }) {//, states, setStates }) {
             </div>
 
             {/* Some room */}
-            <div style={{position: "absolute", top: `${minHeightEm}`}}>
+            <div style={{position: "absolute", top: `${minHeightEm}em`}}>
                 {room}
             </div>
 
             {/* End time selected */}
-            <div style={{position: "absolute", top: `${maxHeightEm}` }}>
+            <div style={{position: "absolute", top: `${maxHeightEm}em` }}>
                 {states.dayTimes[`end${date}`]}
             </div>
         </div>
--- Frontend/OSU-Open-Classroom-Finder/src/helpers/calandarHelpers.js
+++ Frontend/OSU-Open-Classroom-Finder/src/helpers/calandarHelpers.js
@@ -3,8 +3,21 @@ const pageHeightEm = 35;
 const maxHeightEm = pageHeightEm-3;
 const minHeightEm = 3;
 
-function calculateItemPosition(){
-
+const calculateItemPosition = (startTime, endTime, time) => {
+    let totalTime = (new Date(endTime).getHours) - (new Date(startTime.getHours));
+    console.log(getTimeDiff(startTime, endTime));
 }
 
-export {pageHeightEm, maxHeightEm, minHeightEm}; 
+//Get diff between time2 and time1, ie time2-time1
+const getTimeDiff = (time1, time2) => {
+    let h1 = time1.split(':')[0];
+    let h2 = time2.split(':')[0];
+    h1 = time1.includes("PM") && h1 != "12" ? h1+12 : h1;
+    h2 = time2.includes("PM") && h2 != "12" ? h1+12 : h2;
+    h1 = time1.includes("AM") && h1 == "12" ? "0" : h1;
+    h2 = time2.includes("AM") && h2 == "12" ? "0" : h2;
+
+    return h2-h1;
+}
+
+export {pageHeightEm, maxHeightEm, minHeightEm, calculateItemPosition}; 
