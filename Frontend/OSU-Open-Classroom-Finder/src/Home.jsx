dfa73693069a89d2f20279ee2059e6692b78bf91
Samiul Islam <cs.isamiul@gmail.com>
2023-03-07 15:05:37 -0500

    Fixed grid layout in standard desktop mode

--- Frontend/OSU-Open-Classroom-Finder/src/Home.jsx
+++ Frontend/OSU-Open-Classroom-Finder/src/Home.jsx
@@ -9,15 +9,21 @@ function Home() {
     const [room, setRoom] = useState([]);
     const [startWeek, setStartWeek] = useState((new Date().toISOString()).split('T')[0]);
     const [dayTimes, setDayTimes] = useState({});
-    const[options, setOptions] = useState({});
+    const [options, setOptions] = useState({});
 
     const states = { building, room, startWeek, dayTimes, options };
     const setStates = { setBuilding, setRoom, setStartWeek, setDayTimes, setOptions };
 
     return <>
-        <div className="grid gridColumnsMinGap">
-            <Forms states={states} setStates={setStates} dayNames={dayNames}/>
-            <Schedule date={'Monday'} states={states} setStates={setStates}/>
+        <div className="grid" style={{ width: "40vw" }}>
+            <Forms states={states} setStates={setStates} dayNames={dayNames} />
+            <div className="grid" style={{ width: "100vw", position: "relative"}}>
+                <Schedule date={'Monday'} states={states} setStates={setStates}/>
+                <Schedule date={'Tuesday'} states={states} setStates={setStates} />
+                <Schedule date={'Wednesday'} states={states} setStates={setStates} />
+                <Schedule date={'Thursday'} states={states} setStates={setStates} />
+                <Schedule date={'Friday'} states={states} setStates={setStates} />
+            </div>
         </div>
     </>
 }
---
+++ Frontend/OSU-Open-Classroom-Finder/src/helpers/axiosCalls.jsx
@@ -0,0 +1,9 @@
+const axios = require("axios");
+import * as room from "../../../../FakeData/classroom.json";
+
+function GETBuildingRoom(id) {
+    console.log(room);
+    return response;
+};
+
+module.exports = {GETBuildingRoom};
